79f66fc2df81b3e61762102b546d59a9da6b9f2b
Liridon Loku <liridonloku@gmail.com>
2022-07-27 18:21:38 +0200

    Add dynamic inputs for bank accounts

--- src/renderer/components/companyInfo/CompanyInfo.tsx
+++ src/renderer/components/companyInfo/CompanyInfo.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useForm, SubmitHandler, useFieldArray } from 'react-hook-form';
 import { CompanyInfoType } from './companyInfo.types';
 
 type Props = {
@@ -9,93 +11,126 @@ type Props = {
 
 const CompanyInfo: React.FC<Props> = ({ company, update }) => {
   const navigate = useNavigate();
-  const [info, setinfo] = useState(company);
 
-  const updateInfo = () => {
-    update(info);
-  };
+  // Build form
+  const {
+    register,
+    control,
+    handleSubmit,
+    // ADD THIS formState: { errors },
+  } = useForm<CompanyInfoType>();
+
+  // Use field array for dynamic bank accounts
+  const { fields, append, remove } = useFieldArray({
+    name: 'bankAccounts',
+    control,
+  });
+
+  // Fill bank account information on mount
+  useEffect(() => {
+    company.bankAccounts?.forEach((account) => {
+      append({
+        bank: account.bank,
+        accountNumber: account.accountNumber,
+      });
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // Update company info on submit
+  const onSubmit: SubmitHandler<CompanyInfoType> = (data) => update(data);
 
   return (
     <>
-      <h1>Company Info</h1>
-      <button
-        className="btn btn-primary mb-3"
-        type="button"
-        onClick={() => navigate('/')}
-      >
-        Home
-      </button>
-      <form>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          className="form-control mb-2"
-          placeholder="Company Name"
-          value={info.name}
-          onChange={(e) => {
-            setinfo({ ...info, name: e.currentTarget.value });
-          }}
-        />
-        <input
-          type="text"
-          name="id"
-          id="id"
-          className="form-control mb-2"
-          placeholder="Company Id"
-          value={info.id}
-          onChange={(e) => {
-            setinfo({ ...info, id: e.currentTarget.value });
-          }}
-        />
-        <input
-          type="text"
-          name="address"
-          id="address"
-          className="form-control mb-2"
-          placeholder="Company address"
-          value={info.address}
-          onChange={(e) => {
-            setinfo({ ...info, address: e.currentTarget.value });
-          }}
-        />
-        <input
-          type="tel"
-          name="phone"
-          id="phone"
-          className="form-control mb-2"
-          placeholder="Company phone no"
-          value={info.phone}
-          onChange={(e) => {
-            setinfo({ ...info, phone: e.currentTarget.value });
-          }}
-        />
-        <div className="input-group mb-3">
-          <span className="input-group-text">Bank name</span>
+      <div className="container text-center">
+        <h1>Company Info</h1>
+        <button
+          className="btn btn-primary mb-3"
+          type="button"
+          onClick={() => navigate('/')}
+        >
+          Home
+        </button>
+      </div>
+      <div className="container">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <input
             type="text"
-            id="bankName"
-            className="form-control"
-            placeholder="Bank name"
+            {...register('name')}
+            name="name"
+            id="name"
+            className="form-control mb-2"
+            placeholder="Company Name"
+            defaultValue={company.name}
           />
-          <span className="input-group-text">Account no</span>
           <input
             type="text"
-            id="accountNo"
-            className="form-control"
-            placeholder="Account no"
+            {...register('id')}
+            name="id"
+            id="id"
+            className="form-control mb-2"
+            placeholder="Company Id"
+            defaultValue={company.id}
           />
-        </div>
-        <div className="text-center">
-          <button
-            type="button"
-            onClick={updateInfo}
-            className="btn btn-success"
-          >
-            Update Info
-          </button>
-        </div>
-      </form>
+          <input
+            type="text"
+            {...register('address')}
+            name="address"
+            id="address"
+            className="form-control mb-2"
+            placeholder="Company address"
+            defaultValue={company.address}
+          />
+          <input
+            type="tel"
+            {...register('phone')}
+            name="phone"
+            id="phone"
+            className="form-control mb-2"
+            placeholder="Company phone no"
+            defaultValue={company.phone}
+          />
+          {fields.map((item, i) => (
+            <div className="input-group mb-3" key={item.id}>
+              <span className="input-group-text">Bank name</span>
+              <input
+                type="text"
+                {...register(`bankAccounts.${i}.bank`)}
+                className="form-control"
+                defaultValue={item.bank}
+                placeholder="Bank name"
+              />
+              <span className="input-group-text">Account no</span>
+              <input
+                type="text"
+                {...register(`bankAccounts.${i}.accountNumber`)}
+                className="form-control"
+                defaultValue={item.accountNumber}
+                placeholder="Account no"
+              />
+              <button
+                type="button"
+                className="btn btn-danger"
+                onClick={() => remove(i)}
+              >
+                Remove
+              </button>
+            </div>
+          ))}
+          <div className="text-center">
+            <button
+              type="button"
+              onClick={() => append({ bank: '', accountNumber: '' })}
+              className="btn btn-outline-primary me-2"
+            >
+              Add Bank Account
+            </button>
+            <button type="submit" className="btn btn-success">
+              Update Info
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
