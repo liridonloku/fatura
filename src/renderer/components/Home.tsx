8a4eae975a9e60e68728db32a9d8fdcdddb9060b
Liridon Loku <liridonloku@gmail.com>
2022-09-17 15:37:58 +0200

    Add more translations

--- src/renderer/components/Home.tsx
+++ src/renderer/components/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { BsFileEarmarkPlus } from 'react-icons/bs';
+import { t } from 'i18next';
 
 type Props = {
   /**
@@ -16,15 +17,19 @@ const Home: React.FC<Props> = ({ logo }) => {
   const navigate = useNavigate();
   return (
     <div className="container text-center">
-      <h1 className="my-3">Home</h1>
+      <h1 className="my-3">
+        <>{t('home')}</>
+      </h1>
       <button
         className="btn btn-outline-success shadow"
         type="button"
         onClick={() => navigate('/new-invoice')}
       >
         <span className="d-flex align-items-center gap-2 fw-bold">
-          <BsFileEarmarkPlus size={24} />
-          New Invoice
+          <>
+            <BsFileEarmarkPlus size={24} />
+            {t('new-invoice')}
+          </>
         </span>
       </button>
       {logo && (
