58797b7b632c37e2430f1a9e9069902d70eff992
Liridon Loku <liridonloku@gmail.com>
2023-02-25 21:53:14 +0100

    Rewrite with MUI

--- src/renderer/components/Header.tsx
+++ src/renderer/components/Header.tsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 import { CompanyInfoType } from './companyInfo/companyInfo.types';
 
 type Props = {
@@ -8,7 +15,24 @@ type Props = {
 const Header: React.FC<Props> = ({ company }) => {
   return (
     <div className="d-print-none">
-      {company.name || 'Update Company information'}
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              {company.name || 'Update Company information'}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </div>
   );
 };
