039bcbf4c3d239a3253c5360a797bf98ab88f115
Liridon Loku <liridonloku@gmail.com>
2023-02-25 21:26:49 +0100

    Install mui

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 import './i18n/config';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('root')!;
